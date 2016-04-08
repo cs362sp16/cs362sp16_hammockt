e8ab104a33c35474f48346d27a4ca49e3c0f408b
hammockt <hammocktrevor@gmail.com>
2016-04-07 20:21:07 -0700

    Fix Mine

--- dominion.c
+++ dominion.c
@@ -547,43 +547,29 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
 			return gainCard(choice1, state, 0, currentPlayer);//Gain the card
 
-	case gardens:
-	  return -1;
+		case gardens: //this should just be deleted, its not a action card
+			return -1;
 
-	case mine:
-	  j = state->hand[currentPlayer][choice1];  //store card we will trash
+		case mine:
+			//needs to be a treasure card
+			if(state->hand[currentPlayer][choice1] < copper || state->hand[currentPlayer][choice1] > gold)
+				return -1;
 
-	  if (state->hand[currentPlayer][choice1] < copper || state->hand[currentPlayer][choice1] > gold)
-	{
-	  return -1;
-	}
+			//need to gain a treasure card (nothing else)
+			if(choice2 < copper || choice2 > gold)
+				return -1;
 
-	  if (choice2 > treasure_map || choice2 < curse)
-	{
-	  return -1;
-	}
+			//can trade a gold/silver for copper
+			if((getCost(choice2) - 3) > getCost(state->hand[currentPlayer][choice1]))
+				return -1;
 
-	  if ( (getCost(state->hand[currentPlayer][choice1]) + 3) > getCost(choice2) )
-	{
-	  return -1;
-	}
+			gainCard(choice2, state, 2, currentPlayer);
 
-	  gainCard(choice2, state, 2, currentPlayer);
+			//discard card from hand and trash treasure
+			discardCard(handPos, currentPlayer, state, 0);
+			discardCard(choice1, currentPlayer, state, 0);
 
-	  //discard card from hand
-	  discardCard(handPos, currentPlayer, state, 0);
-
-	  //discard trashed card
-	  for (i = 0; i < state->handCount[currentPlayer]; i++)
-	{
-	  if (state->hand[currentPlayer][i] == j)
-		{
-		  discardCard(i, currentPlayer, state, 0);
-		  break;
-		}
-	}
-
-	  return 0;
+			return 0;
 
 	case remodel:
 	  j = state->hand[currentPlayer][choice1];  //store card we will trash
